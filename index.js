67aedb398d233300bf9ef0ef6b96daf51324b989
chris-wamae <chriswamae123@gmail.com>
2022-11-22 18:51:27 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(feet){
+  if(feet <= 400)
+  {return ("This one is on me!")}
+  else if(feet > 2000 && feet < 2501)
+  {return ('I will gladly take your thirty bucks.')}
+  else if(feet >= 2501)
+  {return(`No can do.`)}
+}
+
+//write your code here!
+
+function ternaryCheckCity(city){
+return city === "NYC" ? "Ok, sounds good." : "No go."
+
   // Write your code here!
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
-
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
+  switch(tip)
+  {case "generous": return "Thank you so much.";
+   break;
+   case "not as generous": return "Thank you.";
+   break;
+   default: return "Bye."}
   // Write your code here!
 }
